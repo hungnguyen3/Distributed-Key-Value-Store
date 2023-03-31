fe079264dfd0534d56c1ae5878a88c449dca3539
Hung Nguyen <hachi.codingcat@gmail.com>
2023-03-31 06:51:01 -0700

    Refactor NetworkLayer into NetworkListener and HashRingUpdater

---
+++ src/main/java/com/g6/CPEN431/A7/HashRingUpdater.java
@@ -0,0 +1,42 @@
+package com.g6.CPEN431.A7;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class HashRingUpdater implements Runnable {
+    private HashRing hashRing;
+    private RequestHandlingLayer requestHandlingLayer;
+
+    public HashRingUpdater(HashRing hashRing, RequestHandlingLayer requestHandlingLayer) {
+        this.hashRing = hashRing;
+        this.requestHandlingLayer = requestHandlingLayer;
+    }
+
+    @Override
+    public void run() {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        Runnable updateHashRing = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ArrayList<TransferRequest> transferRequests =  hashRing.updateHashRingUponLatestEpidemicState();
+                    
+                    for (TransferRequest transferRequest : transferRequests) {
+                        requestHandlingLayer.performTransfer(transferRequest);
+                    }
+                } catch (ConcurrentModificationException e) {
+                    System.out.println("ERROR: CHECKING FOR REJOINS" + e);
+                } catch (Exception e) {
+                    System.out.println("ERROR: General exception in updateHashRing" + e);
+                }
+            }
+        };
+        
+        // Update the hash ring every 3 seconds
+        executorService.scheduleAtFixedRate(updateHashRing, 0, 3, TimeUnit.SECONDS);
+    }
+}
